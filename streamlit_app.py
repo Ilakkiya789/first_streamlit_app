5240f4d9f84d24c8c805bab92bf25d4e9408c45e
Ilakkiya789 <131961579+Ilakkiya789@users.noreply.github.com>
2023-05-03 19:25:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
    return fruityvice_normalized
-new section to display fruityvice api response
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 
