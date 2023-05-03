87da04d9f82619fc50fb967c53d8717863846d3b
Ilakkiya789 <131961579+Ilakkiya789@users.noreply.github.com>
2023-05-03 18:23:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Watermelon'])
 
@@ -50,14 +50,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 
-# Display the table on the page.
+
 
 streamlit.dataframe(fruits_to_show)
 
 
 
 
-#create the repeatable code block (called a function)
+
 
 def get_fruityvice_data(this_fruit_choice):
 
@@ -66,7 +66,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
    return fruityvice_normalized
-new section to display fruityvice api response
+
 
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -103,7 +103,7 @@ except URLError as e:
 
 streamlit.header("View Our Fruit List - Add Your Favorites!")
 
-#Snowflake related functions
+
 
 def get_fruit_load_list():
 
@@ -113,7 +113,7 @@ def get_fruit_load_list():
 
       return my_cur.fetchall()
 
-#Add a button to load the fruit 
+
 
 if streamlit.button('Get Fruit List'):
 
